0dfad0c8aa3f3184f53740f1153d7572de242898
ykn <harekazeyukina@protonmail.com>
2021-05-17 15:28:51 +0800

    poi

---
+++ code/Codeforces - Educational Codeforces Round 109 (Rated for Div. 2)/A. Potion-making/sol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+
+
+    return 0;
+}
